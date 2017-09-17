c4d3c670c05f5d945980ae5ffa218fbf41f60297
zutmkr <kruhol@interia.pl>
2017-09-17 17:40:07 +0200

    [PROTOTYPE]: Added basic client-server communication.

---
+++ praca_inz/client.py
@@ -0,0 +1,14 @@
+import socket, pickle
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(('127.0.0.1', 4000))
+
+while True:
+    data = input("Wyślij tekst na serwer: ")
+    data = pickle.dumps(data)
+    server.send(data)
+    data = server.recv(1024)
+    data = pickle.loads(data)
+    print(data)
+
+server.close()

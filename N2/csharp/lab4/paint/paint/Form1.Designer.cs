988a5ed1e939f55b64f56f9c4c46fd091cdc3e0f
Mateusz Krusinski <mateusz.krusinski@synapse.com>
2017-11-22 13:22:54 +0100

    C# lab4

---
+++ N2/csharp/lab4/paint/paint/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_PenColor = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_PenColor
+            // 
+            this.btn_PenColor.Location = new System.Drawing.Point(13, 122);
+            this.btn_PenColor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_PenColor.Name = "btn_PenColor";
+            this.btn_PenColor.Size = new System.Drawing.Size(112, 35);
+            this.btn_PenColor.TabIndex = 0;
+            this.btn_PenColor.Text = "Color";
+            this.btn_PenColor.UseVisualStyleBackColor = true;
+            this.btn_PenColor.Click += new System.EventHandler(this.btn_PenColor_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(936, 35);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(131, 30);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(141, 42);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(777, 635);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 76);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Free draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Circle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Rectangle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 695);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_PenColor);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Mini Paint by Mateusz Krusinski";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_PenColor;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ N2/csharp/lab4/paint/paint/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace paint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics gr_ob;
+        Pen p;
+        bool mouseIsDown, draw_line = true, draw_rectangle, draw_circle;
+        int? initX = null;
+        int? initY = null;
+        int pressedLocationX;
+        int pressedLocationY;
+        int releasedLocationX;
+        int releasedLocationY;
+
+
+        private void btn_PenColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog c = new ColorDialog();
+            if (c.ShowDialog() == DialogResult.OK)
+            {
+                btn_PenColor.BackColor = c.Color;
+                p.Color = c.Color;
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gr_ob.Clear(panel1.BackColor);
+            panel1.BackColor = Color.White;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseIsDown)
+            {
+                if (draw_line)
+                {
+                    gr_ob.DrawLine(p, new Point(initX ?? e.X , initY ?? e.Y), new Point(e.X , e.Y));
+                    initX = e.X;
+                    initY = e.Y;
+                }
+                else if (draw_circle)
+                {
+                    int x = e.X;
+                    int y = e.Y;
+                    Rectangle rectangle = new Rectangle();
+                    PaintEventArgs arg = new PaintEventArgs(gr_ob, rectangle);
+
+                    DrawCircle(arg, x, y, 100, 100);
+                }
+                else if (draw_rectangle)
+                {
+                    gr_ob.DrawRectangle(p,
+                            Math.Min(pressedLocationX, releasedLocationX), // Left
+                            Math.Min(pressedLocationY, releasedLocationY), // Top
+                            Math.Abs(releasedLocationX - pressedLocationX), // Width
+                            Math.Abs(releasedLocationY - pressedLocationY)); // Height
+                }
+            }
+            
+        }
+
+        private void DrawCircle(PaintEventArgs e, int x, int y, int width, int height)
+        {
+            p = new Pen(Color.Red, 3);
+            gr_ob.DrawEllipse(p, x - width / 2, y - height / 2, width, height);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            gr_ob = panel1.CreateGraphics();
+            p = new Pen(Color.Red, 5);
+            //panel1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            draw_line = true;
+            draw_rectangle = false;
+            draw_circle = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            draw_line = false;
+            draw_rectangle = false;
+            draw_circle = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            draw_line = false;
+            draw_rectangle = true;
+            draw_circle = false;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                mouseIsDown = true;
+            pressedLocationX = e.X;
+            pressedLocationY = e.Y;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseIsDown = false;
+            releasedLocationX = e.X;
+            releasedLocationY = e.Y;
+
+        }
+    }
+}

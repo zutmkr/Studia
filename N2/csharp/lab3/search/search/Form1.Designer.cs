6cfbdd524f5f9668cb05c14333224cc849c933f5
Mateusz Krusinski <mateusz.krusinski@synapse.com>
2017-12-12 14:32:04 +0100

    Almost finished

--- N2/csharp/lab3/search/search/Form1.Designer.cs
+++ N2/csharp/lab3/search/search/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
             // 
             // folderBrowserDialog1
@@ -38,7 +42,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(118, 31);
+            this.button1.Location = new System.Drawing.Point(12, 31);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(148, 76);
             this.button1.TabIndex = 0;
@@ -46,16 +50,48 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(181, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(447, 51);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(323, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Copy destination";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(302, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse output folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(640, 195);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -63,6 +99,10 @@
 
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
     }
 }
 
--- N2/csharp/lab3/search/search/Form1.cs
+++ N2/csharp/lab3/search/search/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
@@ -9,7 +10,9 @@ namespace search
     {
 
         string choosen_path;
+        string copy_dest;
         List<string> to_copy = new List<string>();
+
 
 
         public void Search_for_files()
@@ -46,7 +49,20 @@ namespace search
                 }
             }
 
-            //System.Diagnostics.Debug.Print(to_copy[1]);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(to_copy[1]);
+
+            XmlNodeList elemList = doc.GetElementsByTagName("Compile");
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                to_copy.Add(Path.GetDirectoryName(to_copy[1]) + "\\" + elemList[i].Attributes["Include"].Value);
+            }
+            DirectoryInfo di = Directory.CreateDirectory(copy_dest);
+            foreach (string x in to_copy)
+            {
+                File.Copy(x, copy_dest + Path.GetFileName(x));
+            }
+
         }
 
 
@@ -58,7 +74,8 @@ namespace search
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            textBox1.Text = "C:\\kopia\\";
+            copy_dest = "C:\\kopia\\";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -70,6 +87,24 @@ namespace search
             {
                 choosen_path = folderBrowserDialog1.SelectedPath;
                 Search_for_files();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            copy_dest = textBox1.Text;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog2 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog2.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog2.SelectedPath + "\\";
             }
         }
     }

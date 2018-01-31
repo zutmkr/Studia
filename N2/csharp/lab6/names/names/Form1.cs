8cbae26aeb3802ac30712cceafb6b7baa0c1fb19
Mateusz Krusinski <mateusz.krusinski@synapse.com>
2018-01-31 14:58:41 +0100

    Lab6 completed

--- N2/csharp/lab6/names/names/Form1.cs
+++ N2/csharp/lab6/names/names/Form1.cs
@@ -1,21 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace names
 {
     public partial class Form1 : Form
     {
-        string text;
+        public string[] text;
+        List<string> listx2 = new List<string>();
+        List<string> listx3 = new List<string>();
+        AutoCompleteStringCollection source = new AutoCompleteStringCollection();
 
+        public void generate_list_x2()
+        {
+            foreach (var line in text)
+            {
+                var temp = line.Substring(line.LastIndexOf(' ') + 1);
+                char[] chars = {temp[0], temp[1]};
+                var temp2 = new string(chars);
+
+                string result = listx2.FirstOrDefault(x => x.Contains(temp2));
+                if (result != null)
+                {
+                    for (int i = 0; i < listx2.Count; i++)
+                    {
+                        if (listx2[i].Contains(result))
+                            listx2[i] = result + " " + temp;
+                    }
+                }
+                else
+                {
+                    listx2.Add(temp2 + " " + temp);
+                }
+            }
+        }
+
+        public void generate_list_x3()
+        {
+            foreach (var line in text)
+            {
+                var temp = line.Substring(line.LastIndexOf(' ') + 1);
+                if (temp.Length <= 2)
+                    continue;
+                char[] chars = { temp[0], temp[1], temp[2] };
+                var temp2 = new string(chars);
+
+                string result = listx3.FirstOrDefault(x => x.Contains(temp2));
+                if (result != null)
+                {
+                    for (int i = 0; i < listx3.Count; i++)
+                    {
+                        if (listx3[i].Contains(result))
+                            listx3[i] = result + " " + temp;
+                    }
+                }
+                else
+                {
+                    listx3.Add(temp2 + " " + temp);
+                }
+            }
+        }
+
+
+        public string[] read_file()
+        {
+            return File.ReadAllLines(@"nazwiska.txt", Encoding.GetEncoding("Windows-1250"));
+        }
 
         public Form1()
         {
@@ -30,17 +84,50 @@ namespace names
             text = read_file();
             sw.Stop();
             label1.Text = sw.Elapsed.TotalMilliseconds.ToString() + " ms";
+
+            sw = Stopwatch.StartNew();
+            generate_list_x2();
+            sw.Stop();
+            label2.Text = sw.Elapsed.TotalMilliseconds.ToString() + " ms";
+
+            sw = Stopwatch.StartNew();
+            generate_list_x3();
+            sw.Stop();
+            label3.Text = sw.Elapsed.TotalMilliseconds.ToString() + " ms";        
         }
 
-        public string read_file()
-        {
-            return File.ReadAllText(@"nazwiska.txt", Encoding.UTF8);
-        }
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            //TODO
+            
+            if (textBox1.TextLength >=3)
+            {
+                string result = listx3.FirstOrDefault(x => x.Contains(textBox1.Text));
+                if (result != null)
+                {
+                    string[] t_result = result.Split(' ');
+                    t_result = t_result.Skip(1).ToArray();
+                    source.AddRange(t_result);
+                }
+            }
+            else if (textBox1.TextLength == 2)
+            {
+                string result = listx2.FirstOrDefault(x => x.Contains(textBox1.Text));
+                if (result != null)
+                {
+                    string[] t_result = result.Split(' ');
+                    t_result = t_result.Skip(1).ToArray();
+                    source.AddRange(t_result);
+                }      
+            }
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.AutoCompleteCustomSource = source;
+            textBox1.AutoCompleteMode = AutoCompleteMode.Suggest;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
     }
 }
